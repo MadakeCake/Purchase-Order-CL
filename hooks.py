99be08559670bc69c07e840f533b706027ad3b64
MadakeCake <62271847+MadakeCake@users.noreply.github.com>
2025-09-29 19:36:50 -0300

    Update hooks.py

--- hooks.py
+++ hooks.py
@@ -1,10 +1,10 @@
 app_name = "purchase_order_cl"
 app_title = "Purchase Order Chile"
-app_publisher = "Tu Empresa"
+app_publisher = "SINETCY"
 app_description = "Doctype de Orden de Compra adaptado al sistema tributario chileno"
 app_icon = "octicon octicon-file-directory"
 app_color = "grey"
-app_email = "you@example.com"
+app_email = "VPALMA"
 app_license = "MIT"
 
 doctype_js = {
