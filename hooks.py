a013df5edfd09602e646391a6e9b3c1f8962259d
MadakeCake <62271847+MadakeCake@users.noreply.github.com>
2025-09-29 19:07:23 -0300

    Add files via upload

---
+++ hooks.py
@@ -0,0 +1,12 @@
+app_name = "purchase_order_cl"
+app_title = "Purchase Order Chile"
+app_publisher = "Tu Empresa"
+app_description = "Doctype de Orden de Compra adaptado al sistema tributario chileno"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "you@example.com"
+app_license = "MIT"
+
+doctype_js = {
+    "Purchase Order CL": "public/js/purchase_order_cl.js"
+}
